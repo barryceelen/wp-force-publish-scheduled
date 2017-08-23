4ad496c79eb84a5d9743a3a5fff4757084f6caaa
Barry Ceelen <barry@multipop.org>
2017-08-23 20:27:23 +0200

    Initial commit

---
+++ force-publish-scheduled.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Main plugin file
+ *
+ * @package   Force_Publish_Scheduled
+ * @author    Barry Ceelen <b@rryceelen.com>
+ * @license   GPL-3.0+
+ * @link      https://github.com/barryceelen/wp-force-publish-scheduled
+ * @copyright 2017 Barry Ceelen
+ *
+ * Plugin Name: Force Publish Scheduled Posts
+ * Plugin URI:  http://github.com/barryceelen/wp-force-publish-scheduled
+ * Description: Instantly publish scheduled posts via a Bulk Action.
+ * Version:     1.0.0
+ * Author:      Barry Ceelen
+ * Author URI:  https://github.com/barryceelen
+ * Text Domain: force-publish-scheduled
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: /languages
+ */
+
+// Don't load directly.
+defined( 'ABSPATH' ) || die();
+
+if ( is_admin() ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-force-publish-scheduled-admin.php' );
+
+	global $force_publish_scheduled_admin;
+	$force_publish_scheduled_admin = Force_Publish_Scheduled_Admin::get_instance();
+
+	/**
+	 * Load plugin textdomain.
+	 *
+	 * @since 1.0.0
+	 */
+	function myplugin_load_textdomain() {
+		load_plugin_textdomain( 'force-publish-scheduled', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	}
+
+	add_action( 'plugins_loaded', 'myplugin_load_textdomain' );
+}
